c980e21d7d68a38aae4286523b92f5a109221468
LimeTheCoder <limethecoder@gmail.com>
2016-10-29 20:50:48 +0300

    Add controller

--- GuessNumberGame/src/main/java/com/training/numguesser/Controller.java
+++ GuessNumberGame/src/main/java/com/training/numguesser/Controller.java
@@ -1,5 +1,48 @@
 package com.training.numguesser;
 
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Controller {
+    private Model model;
+    private View view;
+
+    private Scanner scanner;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+
+        setScanner(System.in);
+    }
+
+    public void setScanner(InputStream stream) {
+        scanner = new Scanner(stream);
+    }
+
+    public int handleInput() {
+        view.printMessage(View.INPUT_NUMBER);
+        view.printRangeMessage(View.RANGE_HINT,
+                model.getMin(), model.getMax());
+
+        while(!scanner.hasNextInt()) {
+            view.printMessage(View.WRONG_INPUT);
+            view.printMessage(View.INPUT_NUMBER);
+            scanner.next();
+        }
+
+        return scanner.nextInt();
+    }
+
+    public void run() {
+        int n = handleInput();
+
+        while(n != -1 && !model.checkGuess(n)) {
+            n = handleInput();
+        }
+
+        view.printMessage((n == -1) ? View.LOOSE_CASE : View.WIN_CASE);
+        view.printHistory(model.getHistory(), model.getValue());
+    }
 }
--- GuessNumberGame/src/main/java/com/training/numguesser/Main.java
+++ GuessNumberGame/src/main/java/com/training/numguesser/Main.java
@@ -1,13 +1,10 @@
 package com.training.numguesser;
-import java.util.concurrent.ThreadLocalRandom;
-
-// nextInt is normally exclusive of the top value,
-// so add 1 to make it inclusive
-
 
 public class Main {
     public static void main(String ... args) {
-        System.out.println();
-
+        Model model = new Model();
+        View view = new View(System.out);
+        Controller controller = new Controller(model, view);
+        controller.run();
     }
 }
