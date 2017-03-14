54620bcabfe94dd475f57dcbffe3a2e73cb0f9a7
Taras Sakharchuk <limethecoder@gmail.com>
2017-03-14 15:19:06 +0200

    Add request for payment

---
+++ Projects/PointOfSale/src/main/java/com/limethecoder/pos/model/PaymentRequest.java
@@ -0,0 +1,24 @@
+package com.limethecoder.pos.model;
+
+
+public class PaymentRequest {
+    private Sale sale = new Sale();
+    private int coinsPayed = 0;
+
+    public void addItemToBasket(SalesLineItem salesLineItem) {
+        sale.addItemToSale(salesLineItem);
+    }
+
+    public void putCoin(int coin) {
+        coinsPayed += coin;
+    }
+
+    public boolean isPayed() {
+        return getMoneyLeftToPay() == 0;
+    }
+
+    public int getMoneyLeftToPay() {
+        int leftToPay = sale.getTotalPrice() - coinsPayed;
+        return leftToPay < 0 ? 0 : leftToPay;
+    }
+}
--- Projects/PointOfSale/src/main/java/com/limethecoder/pos/model/VendorMachine.java
+++ Projects/PointOfSale/src/main/java/com/limethecoder/pos/model/VendorMachine.java
@@ -1,5 +1,16 @@
 package com.limethecoder.pos.model;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class VendorMachine {
+    private Set<Sale> sales = new HashSet<>();
+
+    public int getTotalIncome() {
+        return sales
+                .stream()
+                .mapToInt(Sale::getTotalPrice)
+                .sum();
+    }
 }
